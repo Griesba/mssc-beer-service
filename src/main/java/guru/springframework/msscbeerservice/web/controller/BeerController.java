e620e94957504b51d7532ccaeffddc8daeb2e6bc
Jean Griesbacher <jnsmplcpt@gmail.com>
2020-05-21 16:11:18 +0200

    Beer JPA entity

--- src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -1,23 +1,29 @@
 package guru.springframework.msscbeerservice.web.controller;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@RestController("/api/v1/beer")
-@RequestMapping
+@RestController
+@RequestMapping(BeerController.BASE_URL)
 public class BeerController {
+
+    public static final String BASE_URL = "/api/v1/beer";
+
     @GetMapping("/{beerId}")
     public ResponseEntity getBeerById(@PathVariable UUID beerId) {
-        return new ResponseEntity(BeerDto.builder().build(), HttpStatus.OK);
+        return new ResponseEntity(
+                BeerDto.builder().id(beerId).beerStyleEnum(BeerStyleEnum.GOSE).build(),
+                HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity create(@RequestBody BeerDto beerDto){
-        return new ResponseEntity(BeerDto.builder().build(), HttpStatus.CREATED);
+        return new ResponseEntity(BeerDto.builder().id(UUID.randomUUID()).build(), HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
---
+++ src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,58 @@
+package guru.springframework.msscbeerservice.web.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getBeerById() throws Exception {
+        mockMvc.perform(get(BeerController.BASE_URL + "/" + UUID.randomUUID())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void create() throws Exception {
+        BeerDto beerDto = BeerDto.builder().build();
+        mockMvc.perform(post(BeerController.BASE_URL)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(objectMapper.writeValueAsString(beerDto)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void update() throws Exception {
+        BeerDto beerDto = BeerDto.builder().build();
+        mockMvc.perform(put(BeerController.BASE_URL + "/" + UUID.randomUUID())
+                    .accept(objectMapper.writeValueAsString(beerDto))
+                    .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
+}
