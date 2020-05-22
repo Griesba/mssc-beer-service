143ef36b88bd40c2fdef2e1733a9a005593e5757
Jean Griesbacher <jnsmplcpt@gmail.com>
2020-05-22 15:00:58 +0200

    Implementing beer service methods

--- src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -1,7 +1,9 @@
 package guru.springframework.msscbeerservice.web.controller;
 
+import guru.springframework.msscbeerservice.services.BeerService;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
 import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -10,26 +12,31 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.UUID;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(BeerController.BASE_URL)
 public class BeerController {
 
     public static final String BASE_URL = "/api/v1/beer";
 
+    private final BeerService beerService;
+
     @GetMapping("/{beerId}")
     public ResponseEntity getBeerById(@PathVariable UUID beerId) {
         return new ResponseEntity(
-                BeerDto.builder().id(beerId).beerStyleEnum(BeerStyleEnum.GOSE).build(),
+                beerService.getById(beerId),
                 HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity create(@Valid @RequestBody BeerDto beerDto){
-        return new ResponseEntity(BeerDto.builder().id(UUID.randomUUID()).build(), HttpStatus.CREATED);
+        return new ResponseEntity(
+                beerService.saveNewBeer(beerDto),
+                HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
     public ResponseEntity update(@PathVariable UUID beerId, @Validated @RequestBody BeerDto beerDto){
-        return new ResponseEntity(BeerDto.builder().build(), HttpStatus.NO_CONTENT);
+        return new ResponseEntity(beerService.update(beerId, beerDto), HttpStatus.NO_CONTENT);
     }
 }
